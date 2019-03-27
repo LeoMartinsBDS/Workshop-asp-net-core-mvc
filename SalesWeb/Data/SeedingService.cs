0245fd6252d5aadd0755dd5e6df4d1b948fcf6b1
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-27 17:25:00 -0300

    seeding service

---
+++ SalesWeb/Data/SeedingService.cs
@@ -0,0 +1,46 @@
+﻿using SalesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWeb.Models.Enums;
+
+namespace SalesWeb.Data
+{
+    public class SeedingService
+    {
+        private SalesWebContext _context;
+
+        public SeedingService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
+            {
+                return; // db has been seeded
+            }
+
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Electronics");
+            Department d3 = new Department(3, "Fashion");
+            Department d4 = new Department(4, "Books");
+
+            Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new DateTime(1998,04,21), 1000.0, d1);
+            Seller s2 = new Seller(2, "Mariazinha", "mariazinha@gmail.com", new DateTime(1990,04,22), 15000.0, d2);
+            Seller s3 = new Seller(3, "Joao", "joao@gmail.com", new DateTime(1994,06,26), 1500.0, d3);
+
+            SalesRecord r1 = new SalesRecord(1, new DateTime(2018, 09, 25), 11000.0, SaleStatus.Billed, s1);
+            SalesRecord r2 = new SalesRecord(2, new DateTime(2018, 10, 02), 24000.0, SaleStatus.Billed, s2);
+            SalesRecord r3 = new SalesRecord(3, new DateTime(2018, 11, 10), 2000.0, SaleStatus.Billed, s3);
+
+            _context.Department.AddRange(d1, d2, d3, d4);
+            _context.Seller.AddRange(s1, s2, s3);
+            _context.SalesRecord.AddRange(r1, r2, r3);
+
+            _context.SaveChanges();
+        }
+    }
+}
