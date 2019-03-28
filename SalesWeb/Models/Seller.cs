27cd13c6df01afd83b770c552c5f1d104775bcf3
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-28 15:22:55 -0300

    SellerViewModel e select de departamentos

--- SalesWeb/Models/Seller.cs
+++ SalesWeb/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SalesWeb.Models
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        public int DepartmentID { get; set; }
 
         public Seller() { }
 
@@ -38,7 +39,7 @@ namespace SalesWeb.Models
 
         public double TotalSales(DateTime dtInicial, DateTime dtFinal)
         {
-            return Sales.Where(s => s.Date >= dtInicial && s.Date <= dtFinal).Sum(s=> s.Amount);
+            return Sales.Where(s => s.Date >= dtInicial && s.Date <= dtFinal).Sum(s => s.Amount);
         }
     }
 }
